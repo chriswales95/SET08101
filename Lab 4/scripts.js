357cde63bd5c26b50ae24cf2fad553a61c396d39
Christopher Wales <chriswales95@outlook.com>
2019-02-05 16:28:29 +0000

    Add initial Lab 4 work

---
+++ Lab 4/scripts.js
@@ -0,0 +1,8 @@
+function onClickHello(){
+  alert("Hello");
+}
+
+function changeColours(){
+  document.getElementById("green").style.color = "green";
+  document.body.style.background = "orange";
+}
